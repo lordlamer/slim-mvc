24a7b59ebcc67b02421182a846a910ea86caad3b
fhabermann <habermann@2im.de>
2014-09-05 21:51:22 +0200

    * added monolog logger

--- lib/Loader/Application.php
+++ lib/Loader/Application.php
@@ -4,6 +4,8 @@ namespace IIM\Loader;
 
 use Zend\Config\Reader\Ini as ConfigReader;
 use Zend\Config\Config;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 /**
  * Application class
@@ -46,6 +48,7 @@ class Application {
 		$this->initSlim();
 		$this->initConfig();
 		$this->initDatabase();
+		$this->initLog();
 	}
 
 	/**
@@ -77,6 +80,21 @@ class Application {
 		$config = $this->app->config;
 
 		// create database connection
-		$db = new \PDO($config->database->dsn, $config->database->user, $config->database->password);
+		$this->app->db = new \PDO($config->database->dsn, $config->database->user, $config->database->password);
+	}
+
+	/**
+	 * bootstrap log
+	 */
+	protected function initLog() {
+		// get config
+		$config = $this->app->config;
+
+		// init logger
+		$logger = new Logger('PIM');
+		$logger->pushHandler(new StreamHandler($config->log->file, $config->log->level));
+
+		// save log
+		$this->app->log = $logger;
 	}
 }
