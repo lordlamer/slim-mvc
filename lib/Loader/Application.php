c1ec4780f02fefb1ec3f8f2b06a25ffe7fc74016
fhabermann <habermann@2im.de>
2014-09-05 21:09:52 +0200

    * init slim and config

--- lib/Loader/Application.php
+++ lib/Loader/Application.php
@@ -2,12 +2,69 @@
 
 namespace IIM\Loader;
 
-class Application {
-	public function __construct($config) {
+use Zend\Config\Reader\Ini as ConfigReader;
+use Zend\Config\Config;
 
+/**
+ * Application class
+ */
+class Application {
+	/**
+	 * @var $configFile
+	 */
+	protected $configFile = null;
+
+	/**
+	 * @var $app
+	 */
+	protected $app = null;
+
+	/**
+	 * constructor
+	 *
+	 * @param string $appConfig application configuration
+	 */
+	public function __construct($appConfig) {
+		// save config
+		$this->configFile = $appConfig;
+
+		// bootstrap
+		$this->bootstrap();
 	}
 
+	/**
+	 * run application
+	 */
 	public function run() {
+		$this->app->run();
+	}
 
+	/**
+	 * boostrap the app
+	 */
+	protected function bootstrap() {
+		$this->initSlim();
+		$this->initConfig();
+	}
+
+	/**
+	 * init slim
+	 */
+	protected function initSlim() {
+		// init slim app
+		$this->app = new \Slim\Slim();
+	}
+
+	/**
+	 * bootstrap config
+	 */
+	protected function initConfig() {
+		// init config
+		$configReader = new ConfigReader();
+		$configData = $configReader->fromFile($this->configFile);
+		$config = new Config($configData, true);
+
+		// save config
+		$this->app->config = $config;
 	}
 }
