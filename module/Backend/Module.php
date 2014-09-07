8b0f0e788600b4a8d85f1fb5c48e7a8556b4829d
fhabermann <habermann@2im.de>
2014-09-07 23:05:21 +0200

    * added module config

---
+++ module/Backend/Module.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Backend;
+
+class Module {
+	public function getAutoloaderConfig() {
+		return array(
+			__NAMESPACE__ => __DIR__ . '/src/'
+		);
+	}
+
+	public function getRouterConfig() {
+		return array(
+			__DIR__ . '/route/'
+		);
+	}
+
+	public function getViewConfig() {
+		return array(
+			__NAMESPACE__ => __DIR__ . '/view/'
+		);
+	}
+}
