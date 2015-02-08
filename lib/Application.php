35b1fb4cd2b8624c939fa2264322cd85b48d2b09
fhabermann <habermann@2im.de>
2015-02-08 21:42:38 +0100

    * drill down slim mvc to do only necessary things

--- lib/Application.php
+++ lib/Application.php
@@ -2,19 +2,24 @@
 
 namespace SlimMVC;
 
-use Zend\Config\Reader\Ini as ConfigReader;
-use Zend\Config\Config;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
 /**
  * Application class
  */
 class Application {
 	/**
-	 * @var $configFile
+	 * @var $loader
 	 */
-	protected $configFile = null;
+	protected $loader = null;
+
+	/**
+	 * @var $modulePath
+	 */
+	protected $modulePath = null;
+
+	/**
+	 * @var $modules
+	 */
+	protected $modules = null;
 
 	/**
 	 * @var $app
@@ -24,11 +29,19 @@ class Application {
 	/**
 	 * constructor
 	 *
-	 * @param string $appConfig application configuration
+	 * @param string $loader Composer Autoloader
+	 * @param string $modulePath path with modules
+	 * @param array $modules array with modules to load
 	 */
-	public function __construct($appConfig) {
-		// save config
-		$this->configFile = $appConfig;
+	public function __construct($loader, $modulePath, $modules) {
+		// loader
+		$this->loader = $loader;
+
+		// module path
+		$this->modulePath = $modulePath;
+
+		// modules
+		$this->modules = $modules;
 
 		// bootstrap
 		$this->bootstrap();
@@ -46,11 +59,6 @@ class Application {
 	 */
 	protected function bootstrap() {
 		$this->initSlim();
-		$this->initConfig();
-		$this->initDatabase();
-		$this->initLog();
-		$this->initSession();
-		$this->initLayout();
 		$this->initModules();
 	}
 
@@ -63,102 +71,21 @@ class Application {
 	}
 
 	/**
-	 * bootstrap config
-	 */
-	protected function initConfig() {
-		// init config
-		$configReader = new ConfigReader();
-		$configData = $configReader->fromFile($this->configFile);
-		$config = new Config($configData, true);
-
-		// save config
-		$this->app->config = $config;
-	}
-
-	/**
-	 * bootstrap database
-	 */
-	protected function initDatabase() {
-		// get config
-		$config = $this->app->config;
-
-		// create database connection
-		$this->app->db = new \PDO($config->database->dsn, $config->database->user, $config->database->password);
-	}
-
-	/**
-	 * bootstrap log
-	 */
-	protected function initLog() {
-		// get config
-		$config = $this->app->config;
-
-		// init logger
-		$logger = new Logger($config->log->ident);
-		$logger->pushHandler(new StreamHandler($config->log->file, $config->log->level));
-
-		// save log
-		$this->app->log = $logger;
-	}
-
-	/**
-	 * init session
-	 */
-	protected function initSession() {
-		session_cache_limiter(false);
-		session_start();
-	}
-
-	/**
-	 * init slim layout
-	 */
-	protected function initLayout() {
-		// get slim app
-		$app = $this->app;
-
-		//\Twig_Autoloader::register();
-		// twig filesystem loader
-		$loader = new \Twig_Loader_Filesystem();
-
-		// load twig with filesystem loader
-		$twig = new \Twig_Environment($loader, array(
-			'debug' => true,
-			'cache' => PROJECT_PATH . '/data/cache'
-		));
-
-		// add global base href
-		$twig->addGlobal('base_href', $app->config->base->href);
-
-		// save twig
-		$app->twig = $twig;
-	}
-
-	/**
 	 * bootstrap modules
 	 */
 	protected function initModules() {
 		// app
 		$app = $this->app;
 
-		// get config
-		$config = $this->app->config;
-
-		// get twig
-		$twig = $app->twig;
-
-		// autoloader
-		$loader = new \Zend\Loader\StandardAutoloader();
-
                 // navigation
                 $navigation = array();
 
-		// for each module
-		foreach($config->modules as $module => $enabled) {
-			// check if module is enabled
-			if(!$enabled)
-				continue;
+		// view paths
+		$viewModules = array();
 
-			require_once($config->base->module_path . $module . "/Module.php");
+		// for each module
+		foreach($this->modules as $module) {
+			require_once($this->modulePath . $module . "/Module.php");
 			$m = "\\$module\\Module";
 			$m = new $m;
 
@@ -167,8 +94,9 @@ class Application {
 				$cfg = $m->getAutoloaderConfig();
 
 				// register module namespace
+				// @see: https://getcomposer.org/doc/01-basic-usage.md#autoloading
 				foreach($cfg as $key => $value) {
-					$loader->registerNamespace($key,  $value);
+					$loader->add($key,  $value);
 				}
 			}
 
@@ -188,10 +116,11 @@ class Application {
 			if(method_exists($m, 'getViewConfig')) {
 				$cfg = $m->getViewConfig();
 
-				// register views
+				// get view paths
 				foreach($cfg as $key => $value) {
-					// add module view path
-					$twig->getLoader()->addPath($value, $key);
+					// get module view path
+					// store path in app to register it with template engine
+					$viewModules[$key] = $value;
 				}
 			}
 
@@ -208,7 +137,10 @@ class Application {
                 // register namespaces
 		$loader->register();
 
+		// save view modules
+		$app->container->singleton('viewModules', $viewModules);
+
                 // save navigation
-                $app->navigation = $navigation;
+                $app->container->singletin('navigation', $navigation);
 	}
 }
--- module/Demo/route/hello.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$app->get('/', function () use ($app) {
-	$msg = new \Demo\Test();
-	echo $app->twig->render("@Demo/example.html", array(
-		'message' => $msg->getMessage(),
-	));
-});
